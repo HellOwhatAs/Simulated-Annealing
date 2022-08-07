a672e1c2c18db20eed2fb82ecc4c4f951e8c76ee
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2022-08-07 08:47:58 +0800

    Update SA.pyi

--- SA/SA.pyi
+++ SA/SA.pyi
@@ -2,22 +2,23 @@ from typing import List, Callable
 
 
 def TSP_new_x(x: List[float], T: float) -> List[float]:
-    ...
+    """A function that returns "new x" in solving TSP problem.\n
+    as parameter `udf_new_x` in `SA`"""
 
 
 class SA:
 
     def __init__(self,
-                 func,
-                 x0,
-                 T_max=100,
-                 T_min=1e-7,
-                 cool_down=0.9,
-                 L=300,
-                 max_stay_counter=150,
-                 lb=List[float],
-                 ub=List[float],
-                 udf_new_x: function = None):
+                 func:Callable[[List[float]],float],
+                 x0:List[float],
+                 T_max:float=100,
+                 T_min:float=1e-7,
+                 cool_down:float=0.9,
+                 L:int=300,
+                 max_stay_counter:int=150,
+                 lb:List[float]=[],
+                 ub:List[float]=[],
+                 udf_new_x: Callable[[List[float],float],List[float]] = None):
         """func: the function to minimize.\n
         x0: initial independent variable.\n
         T_max = 100 : initial temperature.\n
